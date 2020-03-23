85497b5122fc250203ad222279ae0c739da6c23b
Ekaterina <mikhailina.ekaterina@gmail.com>
2020-03-23 12:27:31 +0300

    change gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,8 +34,8 @@ module.exports = {
    options: {
     lang: "ru",
    },
-  }// this (optional) plugin enables Progressive Web App + Offline functionality
+  }, // this (optional) plugin enables Progressive Web App + Offline functionality
   // To learn more, visit: https://gatsby.dev/offline
-  `gatsby-plugin-offline`,
+  // `gatsby-plugin-offline`,
  ],
 }
